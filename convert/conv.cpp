b4a9baa85fd240efc4c71ae7e52826e76e6e056f
Jared Bruni <jaredbruni@gmail.com>
2026-03-24 23:41:19 -0700

    conversion tool

--- convert/conv.cpp
+++ convert/conv.cpp
@@ -6,25 +6,39 @@
 #include "argz.hpp"
 #include <algorithm>
 #include <cstdlib>
+#include <filesystem>
 #include <fstream>
 #include <iostream>
 #include <opencv2/opencv.hpp>
 #include <sstream>
 #include <string>
+#include <string_view>
 #include <vector>
 
-bool convertFiles(const std::vector<std::string> &list_files, std::string_view output, const int &width, const int &height);
+namespace fs = std::filesystem;
+
+bool convertFiles(const std::vector<fs::path> &list_files, std::string_view output, int width, int height);
 
 int main(int argc, char **argv) {
 	Argz<std::string> argz;
-	argz.initArgs(argc, argv).addOptionSingleValue('i', "input file").addOptionSingleValue('f', "image file input").addOptionSingleValue('o', "output file format").addOptionSingleValue('s', "scale resolution formst: WithxHeight").addOptionDoubleValue('I', "input", "input file").addOptionDoubleValue('O', "output", "output format").addOptionDoubleValue('S', "size", "scale resolution").addOptionDoubleValue('F', "file", "input image file");
+	argz.initArgs(argc, argv)
+		.addOptionSingleValue('i', "input file")
+		.addOptionSingleValue('f', "image file input")
+		.addOptionSingleValue('o', "output file format")
+		.addOptionSingleValue('s', "scale resolution formst: WithxHeight")
+		.addOptionDoubleValue('I', "input", "input file")
+		.addOptionDoubleValue('O', "output", "output format")
+		.addOptionDoubleValue('S', "size", "scale resolution")
+		.addOptionDoubleValue('F', "file", "input image file");
 
 	if(argc == 1) {
 		argz.help(std::cout);
 		return 0;
 	}
 
-	std::string input_file, output_format, image_size, image_file;
+	fs::path input_file;
+	fs::path image_file;
+	std::string output_format, image_size;
 
 	try {
 		int value{};
@@ -33,7 +47,7 @@ int main(int argc, char **argv) {
 			switch(value) {
 			case 'i':
 			case 'I':
-				input_file = arg.arg_value;
+				input_file = fs::path(arg.arg_value);
 				break;
 			case 'o':
 			case 'O':
@@ -45,45 +59,55 @@ int main(int argc, char **argv) {
 				break;
 			case 'f':
 			case 'F':
-				image_file = arg.arg_value;
+				image_file = fs::path(arg.arg_value);
 				break;
 			}
 		}
-		if(output_format.length() == 0) {
+
+		if(output_format.empty()) {
 			std::cerr << "Error missing output format arguments...\n";
 			argz.help(std::cout);
 			return 0;
 		}
 
-		if(image_file.length() == 0 && input_file.length() == 0) {
+		if(input_file.empty() && image_file.empty()) {
 			std::cerr << "Error missing input file/list...\n";
 			argz.help(std::cout);
 			return 0;
 		}
-		std::vector<std::string> list_files;
-		if(input_file.length() > 0) {
-			std::fstream file;
-			file.open(input_file, std::ios::in);
-			while(!file.eof()) {
-				std::string line;
-				std::getline(file, line);
-				if(file) {
-					list_files.push_back(line);
+
+		std::vector<fs::path> list_files;
+
+		if(!input_file.empty()) {
+			std::ifstream file(input_file);
+			if(!file) {
+				std::cerr << "Error could not open input list: " << input_file << "\n";
+				return 1;
+			}
+
+			std::string line;
+			while(std::getline(file, line)) {
+				if(!line.empty()) {
+					list_files.emplace_back(line);
 				}
 			}
 		}
-		if(image_file.length() > 0) {
+
+		if(!image_file.empty()) {
 			list_files.push_back(image_file);
 		}
-		if(image_size.length() > 0) {
-			const auto pos{image_size.find("x")};
+
+		if(!image_size.empty()) {
+			const auto pos = image_size.find('x');
 			if(pos == std::string::npos) {
 				std::cerr << "Error use format WidthxHeight ex: 640x360\n";
 				return 0;
 			}
-			const std::string left{image_size.substr(0, pos)};
-			const std::string right{image_size.substr(pos + 1, image_size.length() - pos)};
-			if(convertFiles(list_files, output_format, atoi(left.c_str()), atoi(right.c_str()))) {
+
+			const std::string left = image_size.substr(0, pos);
+			const std::string right = image_size.substr(pos + 1);
+
+			if(convertFiles(list_files, output_format, std::atoi(left.c_str()), std::atoi(right.c_str()))) {
 				std::cout << "image_convert: success.\n";
 			} else {
 				std::cout << "image_convert: failed.\n";
@@ -98,55 +122,60 @@ int main(int argc, char **argv) {
 	} catch(const ArgException<std::string> &e) {
 		std::cerr << "Syntax Error: " << e.text() << "\n";
 	}
+
 	return 0;
 }
 
-bool convertFiles(const std::vector<std::string> &list_files, std::string_view output, const int &width, const int &height) {
-
+bool convertFiles(const std::vector<fs::path> &list_files, std::string_view output, int width, int height) {
 	if(width == 0 || height == 0) {
-		std::cerr << "invalid with/height\n";
+		std::cerr << "invalid width/height\n";
 		return false;
 	}
+
 	int converted{0};
 
-	if(list_files.size() > 0) {
-		for(const auto &i : list_files) {
-			std::ostringstream stream;
-			auto pos{i.rfind(".")};
+	for(const auto &input_path : list_files) {
+		if(!input_path.has_filename())
+			continue;
 
-			if(pos == std::string::npos)
-				continue;
+		cv::Mat inputf = cv::imread(input_path.string());
+		if(inputf.empty())
+			continue;
 
-			std::string fname{i.substr(0, pos)};
-			if(fname.length() > 0) {
-				cv::Mat inputf{cv::imread(i)};
-				if(width != -1 && height != -1) {
-					stream << fname << "." << width << "x" << height << "." << output;
-				} else {
-					stream << fname << "." << output;
-				}
-				if(!inputf.empty()) {
-					const std::string o_file{stream.str()};
-					if(o_file == i)
-						continue;
-					std::cout << i << " -> " << o_file << "\n";
-					try {
-						if(width != -1 && height != -1) {
-							cv::Mat resized;
-							cv::resize(inputf, resized, cv::Size(width, height));
-							cv::imwrite(o_file, resized);
-							converted++;
-						} else {
-							cv::imwrite(o_file, inputf);
-							converted++;
-						}
-					} catch(...) {
-						std::cerr << i << " could not convert to format " << output << "...\n";
-					}
-				}
+		fs::path out_path;
+		std::string ext = ".";
+		ext += output;
+
+		if(width != -1 && height != -1) {
+			std::ostringstream name;
+			name << input_path.stem().string() << "." << width << "x" << height;
+			out_path = input_path.parent_path() / name.str();
+			out_path.replace_extension(ext);
+		} else {
+			out_path = input_path.parent_path() / input_path.stem();
+			out_path.replace_extension(ext);
+		}
+
+		if(fs::absolute(input_path) == fs::absolute(out_path))
+			continue;
+
+		std::cout << input_path << " -> " << out_path << "\n";
+
+		try {
+			if(width != -1 && height != -1) {
+				cv::Mat resized;
+				cv::resize(inputf, resized, cv::Size(width, height));
+				cv::imwrite(out_path.string(), resized);
+				++converted;
+			} else {
+				cv::imwrite(out_path.string(), inputf);
+				++converted;
 			}
+		} catch(...) {
+			std::cerr << input_path << " could not convert to format " << output << "...\n";
 		}
 	}
+
 	std::cout << "image(s) converted: " << converted << "\n";
 	return true;
 }
